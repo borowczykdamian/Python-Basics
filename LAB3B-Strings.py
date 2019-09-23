e86cdcb049659bf00ecf6a214c10e1892c3ee533
borowczykdamian <borowczyk.damian@gmail.com>
2019-09-23 14:51:19 +0200

    Commit

---
+++ LAB3B-Strings.py
@@ -0,0 +1,29 @@
+print("This is a line of text!")
+
+print("This is first line!\nThis is second line!")
+
+# Prints out the " that normally would indicate the end of string
+print("This is some \" text")
+
+# This string is stored in a variable
+phrase = "String in a variable"
+print(phrase)
+print("\n" + phrase + " and concatenated string")
+
+print(phrase.capitalize())
+
+print(phrase.upper())
+
+print(phrase.isupper())
+print(phrase.islower())
+
+print(phrase.upper().isupper())
+
+print(len(phrase))
+print(phrase[2])
+
+print(phrase.index("S"))
+print(phrase.index("i"))
+
+
+
